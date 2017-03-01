09850c28a9d69778f7531b8652e91aac8ff96170
iOSmitchsalcido <mitchsalcido@yahoo.com>
2017-03-01 06:28:27 -0800

    beginning work on AudioRecorderManager

--- PitchPerfectMyVersion/AudioRecorderManager.swift
+++ PitchPerfectMyVersion/AudioRecorderManager.swift
@@ -10,11 +10,38 @@ import Foundation
 import AVFoundation
 
 protocol AudioRecorderManagerDelegate {
-    func audioFileSetupFailure(sender: AudioRecorderManager)
-    func audioPayerFinishedPlaying(sender: AudioRecorderManager)
+    func audioRecorderFinishedRecording(sender: AudioRecorderManager, url: URL?)
 }
 
 class AudioRecorderManager: NSObject, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    // errors enum
+    enum Errors: Swift.Error {
+        case AudioRecorderSetupFailure(String)
+    }
+    
+    func recordAudio() throws {
+        
+        // get path save recorded audio
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        do {
+            try audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        } catch {
+            throw Errors.AudioRecorderSetupFailure("Unable to create audioRecorder")
+        }
+        
+        // create audioRecorder, configure, then start recording
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
     
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
         print("audioRecorderDidFinishRecording")
